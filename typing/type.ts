a99a5377efc84d38c21b32f3df01bb5d033b8006
unknown <corfer@yeah.net>
2023-08-21 19:12:32 +0800

    type defined

---
+++ typing/type.ts
@@ -0,0 +1,42 @@
+//enum
+enum GrantType {
+    AUTHORIZATION_CODE = 'authorization_code'
+}
+
+//request to wechat server
+export interface LoginRequest {
+    appid: string;
+    secret: string;
+    js_code: string;
+    grant_type: GrantType;
+}
+
+//response from wechat server
+export interface LoginResponse {
+    openid: string;
+    session_key: string;
+    unionid: string;
+    errcode: number;
+    errmsg: string;
+}
+
+export class AuthInfo {
+    openid: string;
+    session_key: string;
+    unionid: string;
+
+    constructor(lgRes: LoginResponse) {
+        this.openid = lgRes.openid;
+        this.session_key = lgRes.session_key;
+        this.unionid = lgRes.unionid;
+    }
+}
+
+export class AuthError {
+    errcode: number;
+    errmsg: string;
+    constructor(lgRes: LoginResponse) {
+        this.errcode = lgRes.errcode;
+        this.errmsg = lgRes.errmsg;
+    }
+}
